6cfd42b0608bb5f8b1a9780053fb4df5d2ad43f1
xwRongx <57518285+xwRongx@users.noreply.github.com>
2026-08-25 03:18:04 -0700

    incomplete chart code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { LineChart } from '@mui/x-charts/LineChart';
+
+// Fetch trend data for keyword "database"
+function fetchTrendData() {
+    return fetch('http://localhost:5000/api/trends?keyword=database')
+      .then((response) => {
+        return response.json().then((data) => {
+          console.log('Fetched trend data for "database":', data);
+          return data;
+        }).catch(error => console.error('Error fetching data:', error));
+      });
+}
 
 function App() {
+
+  let trendData = null;
+  fetchTrendData().then((data) => {
+    trendData = data;
+  })
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +35,21 @@ function App() {
         >
           Learn React
         </a>
+        <ul>
+          {trendData.map(item => (
+            <li>{item[0]}</li>
+          ))}
+        </ul>
+        <LineChart
+          xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+          series={[
+            {
+              data: [2, 5.5, 2, 8.5, 1.5, 5],
+            },
+          ]}
+          width={500}
+          height={300}
+        />
       </header>
     </div>
   );
